2a345955ee87b6bbc3b3204da9bef902194ffe98
thatdarkw <69354549+thatdarkw@users.noreply.github.com>
2023-06-17 16:44:25 +0530

    Update stuffup.sh
    Updated according to latest version....

--- stuffup.sh
+++ stuffup.sh
@@ -7,16 +7,16 @@ yellow=$(tput setaf 3)
 reset=$(tput sgr0)
 
 # packages
-tools="python3-pip steghide seclists ghidra zaproxy neofetch chromium htop libimage-exiftool-perl chisel python3-venv bloodhound pipx"
+tools="steghide seclists ghidra zaproxy neofetch chromium htop chisel bloodhound pipx"
 
 # pip2
 pip2="https://bootstrap.pypa.io/pip/2.7/get-pip.py"
 
 # peas
-linpeas="https://github.com/carlospolop/PEASS-ng/releases/download/20220214/linpeas.sh"
-winpeasbat="https://github.com/carlospolop/PEASS-ng/releases/download/20220214/winPEAS.bat"
-winpeas64="https://github.com/carlospolop/PEASS-ng/releases/download/20220214/winPEASx64.exe"
-winpeas86="https://github.com/carlospolop/PEASS-ng/releases/download/20220214/winPEASx86.exe"
+linpeas="https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh"
+winpeasbat="https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS.bat"
+winpeas64="https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64.exe"
+winpeas86="https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx86.exe"
 
 # pspy
 pspy32="https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32"
@@ -51,29 +51,6 @@ else
 	echo -e $yellow"\nAdding pipx to path and installing mitm6..."$reset
 	pipx ensurepath
 	pipx install mitm6
-	
-	echo -e $yellow"\nInstalling evil-winrm..."$reset
-	gem install --user-install evil-winrm
-	while true;
-	do
-		echo $red"evil-winrm is installed for current user. Is gem in your path? If not, do you want me to add it to your .zshrc? Y/N?"$reset
-		read answer
-		if [ $answer == "Y" ] || [ $answer == "y" ]
-		then
-			echo $green"Adding path to .zshrc... You will likely have to restart your terminal to apply changes."$reset
-			echo -e '''\nif which ruby >/dev/null && which gem >/dev/null; then
-    PATH="$PATH:$(ruby -r rubygems -e '\'puts Gem.user_dir\'')/bin"
-fi\n''' >> ~/.zshrc
-			break
-		elif [ $answer == "N" ] || [ $answer == "n" ]
-		then
-			echo $green"Told not to add gem path to .zshrc."$reset
-			break
-		else
-			echo $yellow"Wrong input..."$reset
-			continue
-		fi
-	done
 	
 	if [ -d "$1" ]
 	then
